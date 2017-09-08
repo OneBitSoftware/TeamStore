3939c0f16dcc314fd65adb8c5cba1448b864981f
Radi A <radi.a@live.com.au>
2017-09-08 14:57:42 +0300

    Added an encryption service

---
+++ TeamStore/Interfaces/IEncryptionService.cs
@@ -0,0 +1,24 @@
+﻿namespace TeamStore.Interfaces
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// An interface for an Encryption Service
+    /// </summary>
+    public interface IEncryptionService
+    {
+        /// <summary>
+        /// Encrypts a string using AES.
+        /// </summary>
+        /// <param name="stringToEncrypt">The string/text to encrypt</param>
+        /// <returns>The encrypted cipher</returns>
+        Task<string> EncryptString(string stringToEncrypt);
+
+        /// <summary>
+        /// Decrypts a passed cipher to its original text/string
+        /// </summary>
+        /// <param name="stringToDecrypt">The cipher to be decrypted</param>
+        /// <returns>The decrypted text/ciper</returns>
+        Task<string> DecryptString(string stringToDecrypt);
+    }
+}
---
+++ TeamStore/Services/EncryptionService.cs
@@ -0,0 +1,76 @@
+﻿namespace TeamStore.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
+    using TeamStore.Interfaces;
+
+    public class EncryptionService : IEncryptionService
+    {
+        byte[] a = new byte[] { 123, 254, 212, 114, 235, 116, 127, 198, 163, 252, 112, 214, 131, 111, 227, 168 };
+
+        public async Task<string> DecryptString(string stringToDecrypt)
+        {
+            using (var aesAlg = Aes.Create())
+            {
+                using (var encryptor = aesAlg.CreateEncryptor(a, aesAlg.IV))
+                {
+                    using (var msEncrypt = new MemoryStream())
+                    {
+                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            await swEncrypt.WriteAsync(stringToDecrypt);
+                        }
+
+                        var iv = aesAlg.IV;
+
+                        var decryptedContent = msEncrypt.ToArray();
+
+                        var result = new byte[iv.Length + decryptedContent.Length];
+
+                        Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                        Buffer.BlockCopy(decryptedContent, 0, result, iv.Length, decryptedContent.Length);
+
+                        return Convert.ToBase64String(result);
+                    }
+                }
+            }
+        }
+
+        public async Task<string> EncryptString(string stringToEncrypt)
+        {
+            var fullCipher = Convert.FromBase64String(stringToEncrypt);
+
+            var iv = new byte[16];
+            var cipher = new byte[16];
+
+            Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
+            Buffer.BlockCopy(fullCipher, iv.Length, cipher, 0, iv.Length);
+
+            using (var aesAlg = Aes.Create())
+            {
+                using (var decryptor = aesAlg.CreateDecryptor(a, iv))
+                {
+                    string result;
+                    using (var msDecrypt = new MemoryStream(cipher))
+                    {
+                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (var srDecrypt = new StreamReader(csDecrypt))
+                            {
+                                result = await srDecrypt.ReadToEndAsync();
+                            }
+                        }
+                    }
+
+                    return result;
+                }
+            }
+        }
+    }
+}
