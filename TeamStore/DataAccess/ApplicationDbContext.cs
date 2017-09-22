65df0158a01e977d591f23f1a7fa85be691296b5
Radi A <radi.a@live.com.au>
2017-09-22 09:23:58 +0300

    Fixed database creation in integration tests after clean

--- TeamStore/DataAccess/ApplicationDbContext.cs
+++ TeamStore/DataAccess/ApplicationDbContext.cs
@@ -12,10 +12,16 @@ namespace TeamStore.DataAccess
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, bool ensureFullDbCreated = false) : base(options)
         {
             // EnsureCreated was removed due to EF Migrations, which fail if the DB is already created with EnsureCreated
-            // EnsureCreated builds the DB based on this context, creating all tables. Migrations then fails on migrationBuilder.CreateTable(name: "ApplicationIdentities",
+            // EnsureCreated builds the DB based on this context, creating all tables. 
+            // Migrations then fails on migrationBuilder.CreateTable(name: "ApplicationIdentities", because
+            // the DB is already created with the schema from EnsureCreated.
             if (ensureFullDbCreated)
             {
                 var created = Database.EnsureCreated();
+
+                // Running all tests after a Clean and delete of the bin results in a DB creation
+                // exception. Running again passes. Sleeping seems to resolve it.
+                System.Threading.Thread.Sleep(1000);
             }
         }
 
