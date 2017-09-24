7fc87f07f76c184e0e3ea1d8ebfd75f0c9d5bc45
Radi A <radi.a@live.com.au>
2017-09-24 09:28:07 +0300

    Enhanced the SQLite logging provider and extensions

---
+++ src/TeamStore.Keeper/Services/Logging/SQLiteLoggerExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace TeamStore.Keeper.Services.Logging
+{
+    using Microsoft.Extensions.Logging;
+    using System;
+    using TeamStore.Keeper.DataAccess;
+
+    public static class SQLiteLoggerExtensions
+    {
+        public static ILoggerFactory AddSQLiteLogger<TDbContext, TLogEntity>(
+            this ILoggerFactory factory,
+            IServiceProvider serviceProvider,
+            Func<string, LogLevel, bool> filter = null)
+            where TDbContext : ApplicationDbContext
+            where TLogEntity : Models.Log, new()
+
+        {
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
+
+            factory.AddProvider(new SQLiteLoggerProvider<TDbContext, TLogEntity>(serviceProvider, filter));
+
+            return factory;
+        }
+    }
+}
---
+++ src/TeamStore.Keeper/Services/Logging/SQLiteLoggerProvider.cs
@@ -0,0 +1,29 @@
+﻿namespace TeamStore.Keeper.Services.Logging
+{
+    using Microsoft.Extensions.Logging;
+    using System;
+    using TeamStore.Keeper.DataAccess;
+
+    public class SQLiteLoggerProvider<TDbContext, TLogEntity> : ILoggerProvider
+        where TLogEntity : Models.Log, new()
+        where TDbContext : ApplicationDbContext
+    {
+        private readonly Func<string, LogLevel, bool> _filter;
+        private readonly IServiceProvider _serviceProvider;
+
+        public SQLiteLoggerProvider(IServiceProvider serviceProvider, Func<string, LogLevel, bool> filter)
+        {
+            _filter = filter;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new SQLiteLogger<TDbContext, TLogEntity>("SQLiteLogger", _filter, _serviceProvider);
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
