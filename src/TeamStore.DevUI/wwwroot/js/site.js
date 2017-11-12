0ffe9af2808d9d6574f277ae3e323ac830c6699b
Radi A <radi.a@live.com.au>
2017-11-12 19:48:54 +0200

    fixed password calling click event binding

--- src/TeamStore.DevUI/wwwroot/js/site.js
+++ src/TeamStore.DevUI/wwwroot/js/site.js
@@ -1,51 +1,78 @@
 ﻿"use strict";
 
-function hidePassword() {
+/**
+ * Performs a click event for a password mask
+ * Appends the anti-forgery token in the post message and header.
+ * The click logic is complex:
+ * 1. The mask click event is bound.
+ * 2. on click it is replaced with loading and the ajax request is released. The click handler is removed.
+ * 3. the ajax response replaces the mask body with the result, both fail and success. A timer is kicked off.
+ * 4. the timer returns the mask and rebinds the click event to the clicked span. It appears to be recursive.
+ * @param {any} element
+ */
+function passwordClick(element) {
+    var assetId = element.data("id");
+    var postUrl = "/Projects/GetPassword";
+    var token = $('input[name="__RequestVerificationToken"]').val(); // prepare token
+    var projectId = $("input#projectId").val();
+    var postData = {
+        __RequestVerificationToken: token,
+        formDigest: '2134',
+        projectId: projectId,
+        assetId: assetId
+    };
 
-};
+    // 2. replace with loading, unbind click event and release ajax request with token
+    var containerSpan = element;
+    containerSpan.html("Loading...");
+    containerSpan.removeClass("ob-pointer");
+    containerSpan.off("click");
 
-function bindPasswordLabels() {
-    $("span.ob-passwordLabel").on("click", function () {
-        var assetId = $(this).data("id");
-        var postUrl = "/Projects/GetPassword";
-        var token = $('input[name="__RequestVerificationToken"]').val();
-        var projectId = $("input#projectId").val();
-        var postData = {
-            __RequestVerificationToken: token,
-            formDigest: '2134',
-            projectId: projectId,
-            assetId: assetId
-        };
+    $.ajax({
+        type: 'POST',
+        url: postUrl,
+        data: JSON.stringify(postData),
+        contentType: 'application/json',
+        cache: false,
+        headers: { 'RequestVerificationToken': token },
+        success: function (successResult) {
+            // 3. replace mask with result and start timer
+            containerSpan.html(successResult);
 
-        var containerSpan = $(this);
-        containerSpan.html("Loading...");
-        containerSpan.removeClass("ob-pointer");
-
-        $.ajax({
-            type: 'POST',
-            url: postUrl,
-            data: JSON.stringify(postData),
-            dataType: 'json',
-            contentType: 'application/json',
-            cache: false,
-            headers: { 'RequestVerificationToken': token },
-            success: function (successResult) {
-                containerSpan.html(successResult);
-                containerSpan.off("click");
-
-                setTimeout(function () {
-                    containerSpan.html("******");
-                    containerSpan.addClass("ob-pointer");
-                    bindPasswordLabels();
-                }, 2000);
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                $(this).html(textStatus);
-            }
-        });
+            setTimeout(function () {
+                // 4. return mask and rebind click event
+                containerSpan.html("******");
+                containerSpan.addClass("ob-pointer");
+                containerSpan.on('click', function (e) {
+                    passwordClick($(this));
+                })
+            }, 2000);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            // 3. replace mask with result and start timer
+            containerSpan.html(textStatus);
+            setTimeout(function () {
+                // 4. return mask and rebind click event
+                containerSpan.html("******");
+                containerSpan.addClass("ob-pointer");
+                containerSpan.on('click', function (e) {
+                    passwordClick($(this));
+                })
+            }, 2000);
+        }
     });
 }
 
+/**
+ * Binds the click event on all password labels
+ */
+function bindAllPasswordLabels() {
+    $("span.ob-passwordLabel").on('click', function (e) {
+        passwordClick($(this));
+    });
+};
+
 $(document).ready(function () {
-    bindPasswordLabels();
+    // Bind all password label click functions
+    bindAllPasswordLabels();
 });
