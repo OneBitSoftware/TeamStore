70fcd01e890f0c3642aff751468fbbb3c44b5116
Radi A <radi.a@live.com.au>
2017-11-20 10:20:05 +0200

    A simple reports page

---
+++ src/TeamStore.DevUI/Controllers/ReportsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TeamStore.DevUI.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public ReportsController()
+        {
+
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Reports/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            
+            return View();
+        }
+    }
+}
