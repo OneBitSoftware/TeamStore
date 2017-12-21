da5a20dbf7ef9d6e8702d7cc449e63d77a5b59f2
Kaloyan Asenov <pasariisky@gmail.com>
2017-12-21 18:01:22 +0200

    AssetFactory method now accepts null argument and is backed up by unit test

--- src/TeamStore.DevUI/Factories/AssetFactory.cs
+++ src/TeamStore.DevUI/Factories/AssetFactory.cs
@@ -4,15 +4,17 @@
     using TeamStore.DevUI.ViewModels;
     using TeamStore.Keeper.Models;
 
-    // These method names need to be renamed to follow a consistent pattern
     public static class AssetFactory
     {
         public static List<AssetSearchViewModel> ConvertAssetSearch(List<Asset> assets)
         {
-            var result = new List<AssetSearchViewModel>(assets.Count);
-            foreach (var asset in assets)
+            var result = new List<AssetSearchViewModel>(assets?.Count ?? 0);
+            if (assets != null)
             {
-                result.Add(CreateAssetSearchViewModel(asset));
+                foreach (var asset in assets)
+                {
+                    result.Add(CreateAssetSearchViewModel(asset));
+                }
             }
 
             return result;
---
+++ tests/UnitTests/AssetFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using TeamStore.Factories;
+using TeamStore.Keeper.Models;
+using TeamStore.DevUI.ViewModels;
+using Xunit;
+
+namespace UnitTests
+{
+    public class AssetFactoryTests
+    {
+        public AssetFactoryTests()
+        {
+        }
+
+        [Fact]
+        public void ConvertAssetSearch_ShouldNotFailOnNullArgument()
+        {
+            try
+            {
+                AssetFactory.ConvertAssetSearch(null);
+            }
+            catch (NullReferenceException)
+            {
+                Assert.False(true);
+            }
+
+            Assert.True(true);
+        }
+    }
+}
